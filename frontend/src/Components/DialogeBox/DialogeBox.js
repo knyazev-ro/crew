8f0be41bf03624a1d0a015af9a4e3c58313cb147
knyazevseven@gmail.com <knyazevseven@gmail.com>
2024-11-03 03:55:20 +0300

    change colors to indigo

--- frontend/src/Components/DialogeBox/DialogeBox.js
+++ frontend/src/Components/DialogeBox/DialogeBox.js
@@ -5,7 +5,7 @@ export default function DialogBox({ user, collapsed }) {
   return (
     <div className="flex items-center px-2 hover:bg-gray-900">
       <div className="py-1">
-        <div className="rounded-full min-w-14 min-h-14 bg-stone-200 border hover:border-green-600 transition-all duration-150 hover:border-4">
+        <div className="rounded-full min-w-14 min-h-14 bg-stone-200 border hover:border-indigo-600 transition-all duration-150 hover:border-4">
           {user?.image}
         </div>
       </div>
