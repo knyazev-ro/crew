60077b52ceb3627fda80a2e1eaa902d867ce871c
knyazevseven@gmail.com <knyazevseven@gmail.com>
2024-11-05 00:37:03 +0300

    major changes

---
+++ frontend/src/Message/ChatMessages.js
@@ -0,0 +1,36 @@
+import Message from "./Message";
+
+
+export default function ChatMessages({chat}){
+
+    return (
+        <div>
+        <div className="p-4 space-y-2">
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+        <Message text="Ага" isUser={false} />
+        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
+        <Message text="Че реально?" isUser={true} />
+      </div>
+      </div>
+    );
+}
--- frontend/src/Message/MessagePlace.js
+++ frontend/src/Message/MessagePlace.js
@@ -1,33 +1,27 @@
 import ButtomMessagePlace from "./ButtomMessagePlace";
 import TopMessagePlace from "./TopMessagePlace";
 import Message from "./Message";
+import ChatMessages from "./ChatMessages";
 
 export default function MessagePlace() {
   return (
-    <div className="h-screen min-w-96 sticky w-screen flex-col bg-rich_black overflow-y-scroll overflow-x-hidden scrollbar-hide rounded-t-2xl">
-      <TopMessagePlace></TopMessagePlace>
+    <div className="min-w-96 w-full flex flex-col bg-rich_black rounded-t-2xl justify-between">
+    {/* <div className="z-50">
+      </div>
+      <div className="h-5/6 z-10">
+      </div>
+      <div className="h-1/6 w-full bg-red-500 z-50">
+      </div> */}
 
-    <div
-    className=""
-    >
-      <div className="h-screen p-4 space-y-2 overflow-y-scroll scrollbar-hide">
-        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
-        <Message text="Че реально?" isUser={true} />
-        <Message text="Ага" isUser={false} />
-        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
-        <Message text="Че реально?" isUser={true} />
-        <Message text="Ага" isUser={false} />
-        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
-        <Message text="Че реально?" isUser={true} />
-        <Message text="Ага" isUser={false} />
-        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
-        <Message text="Че реально?" isUser={true} />
-        <Message text="Ага" isUser={false} />
-        <Message text="Прикинь, новый Джокер параша полная" isUser={false} />
-        <Message text="Че реально?" isUser={true} />
+      <div className="h-16 z-50">
+        <TopMessagePlace />
       </div>
+      <div className="h-[calc(100vh-128px)] overflow-y-scroll scrollbar-hide">
+      <ChatMessages />
       </div>
-      <ButtomMessagePlace></ButtomMessagePlace>
+      <div className="rounded-full h-16 z-50">
+      <ButtomMessagePlace />
+      </div>
     </div>
   );
 }
