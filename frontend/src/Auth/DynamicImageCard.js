e0037a4c2ca38d20f44fd53186ab0f89a09dee0f
knyazevseven@gmail.com <knyazevseven@gmail.com>
2024-11-07 01:06:41 +0300

    major changes

--- frontend/src/Auth/DynamicImageCard.js
+++ frontend/src/Auth/DynamicImageCard.js
@@ -1,9 +1,9 @@
 export default function DynamicImageCard() {
   return (
-    <div className="bg-purple_sky-100 w-full h-5/6 -translate-x-7 image-login flex">
-      <div className="absolute bottom-2 left-2 w-2/12 h-28 bg-pink-600 flex">
+    <div className="bg-purple_sky-100 w-full h-5/6 -translate-x-7 image-login flex border-2 border-r-purple_sky-100 border-b-purple_sky-100">
+      <div className="absolute bottom-2 left-2 w-2/12 h-28 bg-pink-600 flex border-2 border-purple_sky-100">
         <div className="flex items-center">
-          <div className="mx-2 h-24 bg-gray-400 w-11/12 flex">
+          <div className="mx-2 h-24 bg-gray-400 w-11/12 flex border-2 border-purple_sky-100">
             <div className="px-3 w-5/6 h-full bg-rich_black flex items-center justify-center">
               <div className="px-1 py-1 w-16 h-16 image-boat "></div>
             </div>
--- frontend/src/Auth/LoginCard.js
+++ frontend/src/Auth/LoginCard.js
@@ -1,5 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import "./styles.css";
+
 export default function LoginCard() {
   return (
-    <div className="bg-rich_black-200 rounded-2xl w-1/2 h-1/2">center</div>
+    <div className="border-2 border-b-purple_sky-100 border-l-purple_sky-100 flex flex-col w-1/2 h-1/2 items-center justify-center bg-rich_black-200 rounded-2xl gap-4 hover:border-t-purple_sky-100 hover:border-r-purple_sky-100 transition-all duration-300 ease-in-out">
+      <div className="gap-4 flex flex-col w-full text-purple_sky-400 items-center justify-center">
+        <div className="flex border-2 border-b-purple_sky-100 border-r-purple_sky-100 -translate-y-10 top-0 h-12 py-1 w-5/6 text-center bg-rich_black text-purple_sky-400 text-lg font-bold gap-4 items-center justify-center">
+          <div className="image-boat-icon bg-purple_sky-400 w-9 h-9"></div>
+          <div className="flex flex-col items-start">
+            <span className="text-sm font-medium">the</span>
+            <span className="-mt-4 text-3xl font-bold">crew </span>
+          </div>
+        </div>
+        <input
+          type="text"
+          placeholder="enter a number or email"
+          className="text-center text-rich_black rounded-xl w-5/6 h-14 bg-purple_sky-400"
+        />
+        <input
+          type="password"
+          placeholder="enter a pass@#$%!&*word"
+          className="text-center text-rich_black rounded-xl w-5/6 h-14 bg-purple_sky-400"
+        />
+      </div>
+
+      <div className="gap-2 flex flex-col w-5/6 h-24 items-center justify-center">
+        <button
+          onClick={() => {}}
+          className="transition-all duration-300 ease-in-out border-2 border-purple_sky-100 bg-rich_black w-4/6 hover:w-5/6 h-14 rounded-2xl hover:text-purple_sky-400"
+        >
+          entry
+        </button>
+        <div className="text-purple_sky-400">
+          doesn't have an account?{" "}
+          <button className="font-semibold" onClick={() => {}}>
+            register
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
