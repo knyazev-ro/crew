d9a52a9ff359073359d59695b582d8e07017151d
knyazevseven@gmail.com <knyazevseven@gmail.com>
2024-11-07 00:01:00 +0300

    major changes login

--- frontend/src/Auth/Auth.js
+++ frontend/src/Auth/Auth.js
@@ -5,15 +5,17 @@ import './styles.css'
 export default function Auth(){
 
     return(
-        <div className='px-1 py-1 bg-red-500 h-screen scrollbar-hide overflow-y-scroll'>
-        <div className="flex bg-rich_black h-screen  rounded-2xl ">
-            <div className="flex bg-rich_black w-1/2 items-center justify-center rounded-2xl">
+        <div className="bg-red-500">
+        <div className='border-red-500 border-4 bg-rich_black h-screen rounded-2xl image-login scrollbar-hide overflow-y-scroll'>
+        <div className="flex h-full hover:backdrop-blur-sm">
+            <div className="flex w-[50vw] items-center justify-center rounded-2xl">
                 <LoginCard />
             </div>
 
-            <div className="bg-rich_black w-1/2 flex items-center justify-center rounded-2xl">
+            <div className=" w-[50vw] flex items-center justify-center rounded-2xl">
                <DynamicImageCard />
             </div>
+        </div>
         </div>
         </div>
     );
--- frontend/src/Auth/DynamicImageCard.js
+++ frontend/src/Auth/DynamicImageCard.js
@@ -1,15 +1,24 @@
 export default function DynamicImageCard() {
   return (
-    <div className="bg-purple_sky-100 w-full h-5/6 -translate-x-7 image-login">
-      <div className="absolute bottom-2 left-2 w-1/3 h-32 bg-red-500 flex items-center">
-        <div className="mx-2 h-2/3 bg-gray-400 w-10/12 flex items-center">
-        <div className="w-24 h-full bg-slate-800 items-center justify-center flex">
-            <div className="w-16 h-16 image-boat"></div>
-        </div>
+    <div className="bg-purple_sky-100 w-full h-5/6 -translate-x-7 image-login flex">
+      <div className="absolute bottom-2 left-2 w-2/12 h-28 bg-pink-600 flex">
+        <div className="flex items-center">
+          <div className="mx-2 h-24 bg-gray-400 w-11/12 flex">
+            <div className="px-3 w-5/6 h-full bg-rich_black flex items-center justify-center">
+              <div className="px-1 py-1 w-16 h-16 image-boat "></div>
+            </div>
 
-        <div className="py-1 px-1 flex flex-wrap">
-        <div className="bg-black text-white text-wrap" >ddfbkfdkjbnkjfdbfdkjhbjhfl</div>
-        </div>
+            <div className="w-full h-full bg-rich_black flex justify-center items-center">
+              <div className="px-1 py-1  text-white">
+                <span className="text-md font-medium">the-</span>
+                <span className="text-3xl font-bold">crew </span>
+                <span className="text-sm font-normal flex flex-col mt-2">
+                  <div className="-mt-4">open source</div>
+                  <div className="-mt-2 px-6">messanger</div>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
